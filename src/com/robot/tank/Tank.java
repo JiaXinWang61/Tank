18fb4f085364aa1a6b66be9b9fd27ce93357e2b5
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-12-05 22:57:12 +0800

    抽象出Tank类 封装相应的属性和方法

---
+++ src/com/robot/tank/Tank.java
@@ -0,0 +1,42 @@
+package com.robot.tank;
+
+import java.awt.*;
+
+public class Tank {
+    private int x, y;
+    private Dir dir = Dir.DOWN;
+    private final int SPEED = 10;
+
+    public Tank(int x, int y, Dir dir) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+        }
+    }
+
+    public Dir getDir() {
+        return dir;
+    }
+
+    public void setDir(Dir dir) {
+        this.dir = dir;
+    }
+}
--- src/com/robot/tank/TankFrame.java
+++ src/com/robot/tank/TankFrame.java
@@ -7,9 +7,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
-    int x = 200, y = 200;
-    Dir dir = Dir.DOWN;
-    private final int SPEED = 10;
+    Tank myTank = new Tank(200, 200, Dir.DOWN);
+
 
     public TankFrame() {
         setSize(800, 600);
@@ -28,22 +27,9 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(x, y, 50, 50);
-        switch (dir) {
-            case LEFT:
-                x -= SPEED;
-                break;
-            case UP:
-                y -= SPEED;
-                break;
-            case RIGHT:
-                x += SPEED;
-                break;
-            case DOWN:
-                y += SPEED;
-                break;
-        }
+        myTank.paint(g);
     }
+
 
     class MyKeyListener extends KeyAdapter {
         boolean bL = false;
@@ -92,20 +78,21 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
 
         private void setMainTankDir() {
             if (bL) {
-                dir = Dir.LEFT;
+               myTank.setDir(Dir.LEFT);
             }
             if (bU) {
-                dir = Dir.UP;
+                myTank.setDir(Dir.UP);
             }
             if (bR) {
-                dir = Dir.RIGHT;
+                myTank.setDir(Dir.RIGHT);
             }
             if (bD) {
-                dir = Dir.DOWN;
+                myTank.setDir(Dir.DOWN);
             }
         }
     }
