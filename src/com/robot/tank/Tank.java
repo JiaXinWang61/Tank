13b477146002d3cf93e1f8a5dc78a3f7c5f4a365
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-12-07 08:14:41 +0800

    按下Ctrl键 tankfire出一颗子弹

--- src/com/robot/tank/Tank.java
+++ src/com/robot/tank/Tank.java
@@ -7,13 +7,15 @@ public class Tank {
     private Dir dir = Dir.DOWN;
     private final int SPEED = 10;
     private boolean moving = false;
+    private TankFrame tf = null;
 
 
-    public Tank(int x, int y, Dir dir) {
+    public Tank(int x, int y, Dir dir, TankFrame tf) {
         super();
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tf = tf;
     }
 
     public boolean isMoving() {
@@ -60,5 +62,9 @@ public class Tank {
 
     public void setDir(Dir dir) {
         this.dir = dir;
+    }
+
+    public void fire() {
+        tf.b = new Bullet(this.x, this.y, this.dir);
     }
 }
